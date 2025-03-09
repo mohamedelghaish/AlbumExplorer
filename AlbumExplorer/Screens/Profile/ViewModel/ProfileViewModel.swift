28fb715fcfbbe1d89db42911324b289f3aedbf03
mohamedelghaish <149378056+mohamedelghaish@users.noreply.github.com>
2025-03-10 01:20:47 +0200

    setup the viewModel for profile screen

--- AlbumExplorer/Screens/Profile/ViewModel/ProfileViewModel.swift
+++ AlbumExplorer/Screens/Profile/ViewModel/ProfileViewModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Combine
+import Moya
+
+class ProfileViewModel {
+    private let apiProvider = MoyaProvider<APIService>()
+    @Published var user: User?
+    @Published var albums: [Album] = []
+    
+    func fetchUserData() {
+        apiProvider.fetchData(.getUsers, responseType: [User].self)
+            .map { $0.randomElement() }
+            .replaceError(with: nil)
+            .assign(to: &$user)
+    }
+    
+    func fetchAlbums(userId: Int) {
+        apiProvider.fetchData(.getAlbums(userId: userId), responseType: [Album].self)
+            .replaceError(with: [])
+            .assign(to: &$albums)
+    }
+}
