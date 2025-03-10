4b4e13f36f44851a8a2e7941441bea1b8574ca78
mohamedelghaish <149378056+mohamedelghaish@users.noreply.github.com>
2025-03-10 17:39:25 +0200

    setup albumViewModel

--- AlbumExplorer/Screens/Album/ViewModel/AlbumViewModel.swift
+++ AlbumExplorer/Screens/Album/ViewModel/AlbumViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import Combine
+import Moya
+
+class AlbumViewModel {
+    private let apiProvider = MoyaProvider<APIService>()
+    @Published var photos: [Photo] = []
+    @Published var filteredPhotos: [Photo] = []
+
+    private var cancellables = Set<AnyCancellable>()
+
+//    func fetchPhotos(albumId: Int) {
+//        apiProvider.fetchData(.getPhotos(albumId: albumId), responseType: [Photo].self)
+//            .sink(receiveCompletion: { _ in }, receiveValue: { [weak self] photos in
+//                self?.photos = photos
+//                self?.filteredPhotos = photos
+//                
+//            })
+//            .store(in: &cancellables)
+//    }
+    func fetchPhotos(albumId: Int) {
+        apiProvider.fetchData(.getPhotos(albumId: albumId), responseType: [Photo].self)
+            .receive(on: DispatchQueue.main) // Ensure UI updates on the main thread
+            .sink(receiveCompletion: { _ in }, receiveValue: { [weak self] photos in
+                self?.photos = photos
+                self?.filteredPhotos = photos
+            })
+            .store(in: &cancellables)
+    }
+
+
+    func filterPhotos(query: String) {
+        filteredPhotos = query.isEmpty ? photos : photos.filter { $0.title.lowercased().contains(query.lowercased()) }
+    }
+}
